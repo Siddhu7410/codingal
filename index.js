48a6c0b266d888e0374c12d90be43da956815f14
siddhu 7410 <suresharumugam96@gmail.com>
2025-07-12 01:44:30 -0400

    javascript lets goooooooooo

---
+++ index.js
@@ -0,0 +1,31 @@
+function restorethethrone(){
+    console.log("I may have had a rough start")
+    console.log("But imma keep goin and show who i am again")
+}
+restorethethrone()
+restorethethrone()
+restorethethrone()
+restorethethrone()
+restorethethrone()
+restorethethrone()
+restorethethrone()
+restorethethrone()
+restorethethrone()
+restorethethrone()
+restorethethrone()
+restorethethrone()
+
+function sum(a ,b ,c){
+    var result = a + b + c
+    console.log(result)
+}
+
+sum(4,5,9)
+sum(1000,9099,55555558)
+
+function multiply(a,b,c){
+    var r = a*b*c
+   return r
+}
+var k= multiply(2,5,5)
+console.log(k)
