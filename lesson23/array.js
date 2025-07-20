1a406b530063274102cdd590491e26692af04c36
Siddhesh <thunder69000@gmail.com>
2025-07-20 02:11:01 -0400

    lolboi

---
+++ lesson23/array.js
@@ -0,0 +1,18 @@
+const a =['kratos','sheldon','physics','superman','fortnite']
+console.log(a[0])
+console.log(a[2])
+console.log(a[1])
+   const boolarray=[900,87878,87687687687687,874563894756837456384756]
+   console.log(boolarray[1])
+
+console.log(a.join('_'))
+a.pop()
+function sum (a,b,c){
+    return a+b+c
+}
+function average(a,b,c){
+    return sum(a,b,c)/3
+
+}
+const result=average(4,8,20)
+console.log(result)
