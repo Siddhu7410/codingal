af6fb3c57e50b783fb34513a06ae955e61927a33
Siddhesh <thunder69000@gmail.com>
2025-08-03 02:02:24 -0400

    noice

---
+++ lesson27/activity.js
@@ -0,0 +1,15 @@
+var a =[1,3,5,66,7,8,9,990,-22,44,4444,3453]
+a.sort(function(a,b){ return a-b})
+console.log(a)
+a.sort(function(a,b){return b-a})
+console.log(a)
+a.map(e=>e*10)
+console.log(a.map(e=>e*10))
+console.log(a)
+function haha(){
+    var x=33
+    var y=9
+    var z=77
+    var a= eval('x+y+z')
+    var b=eval('2*8*6*x')
+}haha()
