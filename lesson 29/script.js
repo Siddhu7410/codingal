a8700c1dbf521836f025fa469dcddd6d2deea55a
Siddhesh <thunder69000@gmail.com>
2025-08-10 02:15:35 -0400

    nice

---
+++ lesson 29/script.js
@@ -0,0 +1,22 @@
+//json
+var a={name:'Siddhesh',age:15,iq:'extremely high'}
+console.log(a)
+var b=JSON.stringify(a)
+console.log(b)
+var c=JSON.parse(b)
+console.log(c)
+
+console,log('startinggggg')
+setTimeout(()=>{
+    console.log('work done hurray')
+
+},5000)
+console.log(end)
+var p = new Promise((resolve,reject)=>{
+    setTimeout(()=>{resolve('hurray boi')},7000)
+
+        
+
+})
+
+p.then((data) => console.log(data)).catch(err=>console.log('ok ok noi',err))
