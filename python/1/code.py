0cedddaee6c593102d6db423d7539e4851bb8bbc
Siddhesh <thunder69000@gmail.com>
2025-09-13 02:05:01 -0400

    lolboi69

--- python/1/code.py
+++ python/1/code.py
@@ -5,3 +5,13 @@ b = 'Motivation comes from within'
 c = 99.99
 d = True
 e = False
+
+a=str(a)
+print(a)
+
+print(103//10)
+print(2**10)
+print(100/4)
+print(101%10)
+x=input("enetr the message you want to send to your friend")
+print(f'your input is {x}')
