91978e6f27dcae287decece3c27614fa65d6697a
Siddhu7410 <suresharumugam96@gmail.com>
2025-09-28 01:07:11 -0400

    lol

---
+++ python/1/folre2.py/file22.py
@@ -0,0 +1,16 @@
+import turtle
+sc=turtle.Screen()
+
+sc.bgcolor("yellow")
+sc.setup(400,600)
+
+turtle.title("Welcome to the world of turtle mamey" )
+
+t=turtle.Turtle()
+
+for i in range(333):
+    t.forward(100)
+    t.right(120)
+    
+
+    turtle.done()
