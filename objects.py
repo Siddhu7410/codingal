ed38e6b1b655c227d4b0b220a292b5be82baea9d
Siddhu7410 <suresharumugam96@gmail.com>
2025-10-05 02:01:48 -0400

    gta6

---
+++ objects.py
@@ -0,0 +1,15 @@
+class top_games:
+    def __init__(self,name,genre,price,releasedate):
+            self.name=name
+            self.subjects=genre
+            self.price=price
+            self.releasedate=releasedate
+
+games_obj = top_games('GTA6','action',8000,'MAY2026')
+
+
+print(games_obj.name)
+
+
+
+
