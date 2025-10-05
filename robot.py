6f11297218a9f7d8d5dd973f3dadaffb34b78b6e
Siddhu7410 <suresharumugam96@gmail.com>
2025-10-05 02:18:04 -0400

    CHITTI THE ROBOT

---
+++ robot.py
@@ -0,0 +1,15 @@
+class the_enthiran:
+    def __init__(self,name,memory,price,specieskind):
+            self.name=name
+            self.memory=memory
+            self.price=price
+            self.specieskind=specieskind
+
+ROBOT_obj = the_enthiran('CHITTI','MEMORY1TERABYTE',1556677456,'HUMANOID')
+
+
+print(ROBOT_obj.name)
+
+
+
+
